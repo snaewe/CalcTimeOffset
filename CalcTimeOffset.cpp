bb07ed2c3e4b783bbf98f73f5dfe95d104c3ac24
Stefan Naewe <stefan.naewe@gmail.com>
2012-07-13 17:24:58 +0200

    intial commit
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

---
+++ CalcTimeOffset.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : CalcTimeOffset.cpp
+// Author      : Stefan Naewe
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "HELO" << endl; // prints HELO
+  return 0;
+}
