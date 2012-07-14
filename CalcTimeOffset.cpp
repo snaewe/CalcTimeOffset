f99fb15b9af26b2323f6df0e9964c753ebbac599
Stefan Naewe <stefan.naewe@gmail.com>
2012-07-14 11:34:38 +0200

    empty implementation of time calc. functions
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

--- CalcTimeOffset.cpp
+++ CalcTimeOffset.cpp
@@ -7,10 +7,27 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+using namespace boost::posix_time;
+
+struct TimeOffset
+{
+  unsigned int hour, minute;
+};
+
+ptime get_local_time(ptime, unsigned int, unsigned int);
+ptime get_utc_time(ptime, unsigned int, unsigned int);
 
 int main()
 {
-  cout << "HELO" << endl; // prints HELO
+  TimeOffset offset = { 0, 20 };
+
+  ptime basetime = second_clock::local_time();
+
+  ptime local_start_time = get_local_time(basetime, offset.hour, offset.minute);
+  ptime utc_start_time = get_utc_time(basetime, offset.hour, offset.minute);
+
   return 0;
 }
