cd78ffcab699e9adc21c446899fe79e646d79416
Stefan Naewe <stefan.naewe@gmail.com>
2012-07-19 11:12:49 +0100

    do some time calculations
    Signed-off-by: Stefan Naewe <stefan.naewe@gmail.com>

--- CalcTimeOffset.cpp
+++ CalcTimeOffset.cpp
@@ -7,27 +7,67 @@
 //============================================================================
 
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 
 #include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/date_time/c_local_time_adjustor.hpp>
 
 using namespace boost::posix_time;
 
 struct TimeOffset
 {
+  TimeOffset(unsigned int theHour, unsigned int theMinute)
+    :hour(theHour)
+    ,minute(theMinute)
+  {}
+
   unsigned int hour, minute;
 };
 
-ptime get_local_time(ptime, unsigned int, unsigned int);
-ptime get_utc_time(ptime, unsigned int, unsigned int);
+ptime get_local_time(ptime utcTime)
+{
+  typedef boost::date_time::c_local_adjustor<ptime> local_adj;
+  return local_adj::utc_to_local(utcTime);
+}
+
+ptime offset_time(ptime theTime, TimeOffset const& offs)
+{
+  return theTime + hours(offs.hour) + minutes(offs.minute);
+}
+
+TimeOffset get_offset(ptime timeOne, ptime timeTwo)
+{
+  time_duration d = timeTwo - timeOne;
+  return TimeOffset(d.hours(), d.minutes());
+}
+
+std::ostream&
+operator<<(std::ostream& ostr, TimeOffset const& offs)
+{
+  std::ostringstream sstr;
+  sstr << offs.hour << ":" << std::setw(2) << std::setfill('0') << offs.minute << 'h';
+  ostr << sstr.str();
+  return ostr;
+}
 
 int main()
 {
-  TimeOffset offset = { 0, 20 };
+  TimeOffset offset(0, 20);
 
-  ptime basetime = second_clock::local_time();
+  ptime basetime = second_clock::universal_time();
 
-  ptime local_start_time = get_local_time(basetime, offset.hour, offset.minute);
-  ptime utc_start_time = get_utc_time(basetime, offset.hour, offset.minute);
+  ptime local_start_time = get_local_time(basetime);
 
+  ptime in_two_hours = offset_time(basetime, TimeOffset(2, 0));
+
+  std::cout
+    << to_simple_string(basetime) << " + 2:00h = " << to_simple_string(in_two_hours) << " (UTC) == "
+    << to_simple_string(get_local_time(in_two_hours)) << " (LOCAL)"
+    << std::endl;
+
+  std::cout
+    << to_simple_string(in_two_hours) << " - 2:00h = " << get_offset(basetime, in_two_hours)
+    << std::endl;
   return 0;
 }
